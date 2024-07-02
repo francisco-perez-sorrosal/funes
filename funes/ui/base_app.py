1fc9dcd84f94d01b368d67b3b2efcbf114fd7708
Francisco Perez-Sorrosal <fperezsorrosal@gmail.com>
2024-07-02 18:32:03 +0000

    More cleaning stuff

--- funes/ui/base_app.py
+++ funes/ui/base_app.py
@@ -1,20 +1,5 @@
-
-import importlib.util
-import multiprocessing
-import os
-import pathlib
-
-from typing import Optional, Sequence
-
-import dspy
 import streamlit as st
-import pandas as pd
 
-from dspy.datasets.hotpotqa import HotPotQA
-from dspy.evaluate import Evaluate
-from dspy.teleprompt import BootstrapFewShotWithRandomSearch
-from dsp.utils import flatten, deduplicate
-from funes.interactions import FactualSortQA
 from funes.lang_utils import models, get_llm
 
 
@@ -36,13 +21,12 @@ lm = st.session_state.get("lm")
 if lm is None:
     st.sidebar.markdown("## Select LLM")
     with st.sidebar.form('select_lm_form'):
-        # Dropdown box for selecting options
-        provider_option = st.selectbox("Select a provider", providers, index=0)
-        provider = provider_option
+        # Dropdown box for selecting provider options
+        provider = st.selectbox("Select a provider", providers, index=0)
         model = None
         if provider != "HF":
-            selected_option = st.selectbox("Select an option", model_options, index=len(model_options)-1)
-            model = models.get(selected_option, "")
+            selected_model = st.selectbox("Select a model", model_options, index=len(model_options)-1)
+            model = models.get(selected_model, "")
         if model == "":
             st.warning("No model selected")
             st.stop() 
@@ -53,18 +37,16 @@ if lm is None:
             lm = get_llm(model, provider, port=8081)
             st.session_state["lm"] = lm
             st.sidebar.info(f"LM {lm} selected")
-            st.experimental_rerun()
+            st.rerun()
         else:
             st.sidebar.warning(f"No LM selected")
-    
 else:
-    
     st.sidebar.info(f"LM {lm} selected")
     with st.sidebar.form('reset_lm_form'):
         reset_btn = st.form_submit_button('Reset LM')
         if reset_btn:
             st.sidebar.warning("LM reset")
             del st.session_state["lm"]
-            st.experimental_rerun()
+            st.rerun()
 
 pg.run()
--- funes/ui/retriever_ui.py
+++ funes/ui/retriever_ui.py
@@ -23,22 +23,12 @@ lm = st.session_state.get("lm")
 if not lm:
     st.warning("No model selected")
     st.stop()
-
-# with st.form('retriever'):
-    # text = st.text_area('Enter text:', 'Retrieve the ReAct paper from arxiv 2210.03629')
-    
-    # submitted = st.form_submit_button('Submit')
-    
+   
 retriever = RetrieverAgent(lm)
-    
-    # st.write(type(retriever.get_graph()))
     
 st.image(retriever.get_graph().draw_png())
 
 if prompt := st.chat_input('Retrieve the ReAct paper from arxiv 2210.03629'):
     st.chat_message("user").write(prompt)
     for event in retriever(prompt):
-        print_event(event, _printed=set(), streamlit=True)
-        
-
-        
+        print_event(event, _printed=set(), streamlit=True)     
